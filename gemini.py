d79598e1b86450140d7791dfc8196392e95a54ea
MbuviM <mwendembuvi10@gmail.com>
2024-07-14 18:04:10 +0300

    RAG Application with Gemini

--- gemini.py
+++ gemini.py
@@ -1,13 +1,15 @@
-import streamlit as st
+from flask import Flask, request, jsonify, render_template
 import os
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain.prompts import PromptTemplate
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain.chains.retrieval import create_retrieval_chain
+from langchain.chains import create_retrieval_chain
 from langchain.vectorstores import Chroma
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from dotenv import load_dotenv
+
+app = Flask(__name__)
 
 # Load environment variables from .env file
 load_dotenv()
@@ -31,8 +33,8 @@ text_splitter = RecursiveCharacterTextSplitter(
 )
 chunks = text_splitter.split_documents(docs)
 
- # Initialize VectorDB and Retriever
-persist_directory = "chroma_db"  # Specify a directory for Chroma to persist data
+# Initialize VectorDB and Retriever
+persist_directory = "./chroma"  # Specify a directory for Chroma to persist data
 vectordb = Chroma.from_documents(chunks, embeddings, persist_directory=persist_directory)
 vectordb.persist()
 
@@ -52,23 +54,15 @@ prompt = PromptTemplate.from_template(template)
 combine_docs_chain = create_stuff_documents_chain(llm, prompt)
 retrieval_chain = create_retrieval_chain(retriever, combine_docs_chain)
 
-# Streamlit app
-def main():
-    st.title("The Explainer")
-    st.write("Ask a question about the EACC National Survey Report 2023:")
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-    # User input
-    user_input = st.text_input("Enter your question here:")
-
-    if st.button("Get Answer"):
-        if user_input:
-            st.write(f"Processing question: {user_input}")
-            # Invoke the retrieval chain
-            response = retrieval_chain.invoke({"input": user_input})
-            # Display the answer to the question
-            st.write("Answer:", response["answer"])
-        else:
-            st.write("Please enter a question.")
+@app.route('/get_answer', methods=['POST'])
+def get_answer():
+    user_input = request.form['user_input']
+    response = retrieval_chain.invoke({"input": user_input})
+    return jsonify(response["answer"])
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
